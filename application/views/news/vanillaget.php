628cc12c0122f785dcb74104b2c248b514189522
apcjlquesada <52223592+apcjlquesada@users.noreply.github.com>
2021-06-13 01:12:03 +0800

    updated routes and added sample javascript calls to REST API

---
+++ application/views/news/vanillaget.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <script>
+                const request = new XMLHttpRequest();
+                const url = 'http://localhost/codeigniter/index.php/newsapi';
+                request.open("GET", url);
+                request.send();
+
+                request.onload = (e) => {
+                    alert(request.response);
+                }
+        </script>
+    </body>
+</html>
